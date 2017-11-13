c661cca4f9c3461393272686f671f9a5b88755ba
Stone <g18006069041@gmail.com>
2017-11-13 15:22:17 +0800

    Revert "ios端bug修复"
    This reverts commit a2d5616b86375c73ca1cdca1a8afe9cdcbb8ff6b.

--- ios/RCTCameraManager.m
+++ ios/RCTCameraManager.m
@@ -10,7 +10,6 @@
 #import <AVFoundation/AVFoundation.h>
 #import <ImageIO/ImageIO.h>
 #import "RCTSensorOrientationChecker.h"
-#import "UIImage+Categorys.h"
 
 @interface RCTCameraManager ()
 
@@ -102,13 +101,7 @@ RCT_EXPORT_MODULE();
                @"720p": @(RCTCameraCaptureSessionPreset720p),
                @"AVCaptureSessionPreset1280x720": @(RCTCameraCaptureSessionPreset720p),
                @"1080p": @(RCTCameraCaptureSessionPreset1080p),
-               @"AVCaptureSessionPreset1920x1080": @(RCTCameraCaptureSessionPreset1080p),
-               @"preview":@(RCTCameraCaptureSessionPresetPreview),
-               @"customLow":@(RCTCameraCaptureSessionPresetCustomLow),
-               @"customNormal":@(RCTCameraCaptureSessionPresetCustomNormal),
-               @"customDefault":@(RCTCameraCaptureSessionPresetCustomDefault),
-               @"customMid":@(RCTCameraCaptureSessionPresetCustomMid),
-               @"customHigh":@(RCTCameraCaptureSessionPresetCustomHigh),
+               @"AVCaptureSessionPreset1920x1080": @(RCTCameraCaptureSessionPreset1080p)
                },
            @"CaptureTarget": @{
                @"memory": @(RCTCameraCaptureTargetMemory),
@@ -167,26 +160,6 @@ RCT_CUSTOM_VIEW_PROPERTY(captureQuality, NSInteger, RCTCamera) {
     case RCTCameraCaptureSessionPreset480p:
       qualityString = AVCaptureSessionPreset640x480;
       break;
-          
-    case RCTCameraCaptureSessionPresetPreview:
-      qualityString=AVCaptureSessionPreset3840x2160;
-      break;
-    case RCTCameraCaptureSessionPresetCustomLow:
-      qualityString=AVCaptureSessionPreset352x288;
-      break;
-    case RCTCameraCaptureSessionPresetCustomMid:
-      qualityString=AVCaptureSessionPreset1280x720;
-      break;
-    case RCTCameraCaptureSessionPresetCustomHigh:
-      qualityString=AVCaptureSessionPresetiFrame1280x720;
-      break;
-    case RCTCameraCaptureSessionPresetCustomNormal:
-      qualityString=AVCaptureSessionPreset640x480;
-      break;
-    case RCTCameraCaptureSessionPresetCustomDefault:
-      qualityString=AVCaptureSessionPreset640x480;
-      break;
-
   }
 
   [self setCaptureQuality:qualityString];
@@ -623,31 +596,6 @@ RCT_EXPORT_METHOD(hasFlash:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRej
 
           // create cgimage
           CGImageRef cgImage = CGImageSourceCreateImageAtIndex(source, 0, NULL);
-            if([[options allKeys] containsObject:@"watermark"]&&iii){
-                NSString *value=[options objectForKey:@"watermark"];
-                if(value==nil){
-                    value=@"";
-                }
-                UIImage* image = [UIImage imageWithCGImage: cgImage];
-                AVCaptureVideoOrientation curDeviceOrientation = (AVCaptureVideoOrientation)[[UIDevice currentDevice] orientation];
-                NSInteger orientation=-1;
-                switch (curDeviceOrientation) {
-                    case AVCaptureVideoOrientationPortrait:
-                        orientation=UIImageOrientationUp;
-                        break;
-                    case AVCaptureVideoOrientationPortraitUpsideDown:
-                        orientation=UIImageOrientationDown;
-                        break;
-                    case AVCaptureVideoOrientationLandscapeRight:
-                        orientation=UIImageOrientationLeft;
-                        break;
-                    case AVCaptureVideoOrientationLandscapeLeft:
-                        orientation=UIImageOrientationRight;
-                        break;
-                }
-                UIImage* newImage=[image createImage:image.size text:value orientation:orientation];
-                cgImage=newImage.CGImage;
-            }
 
           // Rotate it
           CGImageRef rotatedCGImage;
@@ -696,7 +644,7 @@ RCT_EXPORT_METHOD(hasFlash:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRej
 
           [self saveImage:rotatedImageData target:target metadata:imageMetadata resolve:resolve reject:reject];
 
-//          CGImageRelease(rotatedCGImage);要注释，不然报错
+          CGImageRelease(rotatedCGImage);
         }
         else {
           reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(error.description));
